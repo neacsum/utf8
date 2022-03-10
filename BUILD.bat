d1b7142b2774f51207779d74eaa97b260be5a015
Mircea Neacsu <mircea@neacsu.net>
2022-03-10 13:48:39 -0500

    Can build when root path contains a space

--- BUILD.bat
+++ BUILD.bat
@@ -4,6 +4,5 @@ rem  Master build script. Execute this script to create all output libraries
 rem  Environment variable DEV_ROOT points to the root of development tree.
 rem
 
-call mklinks.bat
 call setup.bat
 if "%~1"=="" (msbuild build.proj) else (msbuild -target:%1 build.proj)
