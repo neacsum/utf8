8ed64c7c5d6bf43167021bf45087a42d2e8e3d2b
Mircea Neacsu <mircea@neacsu.net>
2022-03-22 12:50:15 -0400

    Combined setup and build scripts

--- BUILD.bat
+++ BUILD.bat
@@ -1,8 +1,25 @@
-@echo off
+rem @echo off
 rem
 rem  Master build script. Execute this script to create all output libraries
 rem  Environment variable DEV_ROOT points to the root of development tree.
 rem
 
-call setup.bat
+rem
+rem setup environment for Visual Studio 2017 and later
+rem
+
+rem VS2017U2 contains vswhere.exe
+if "%VSWHERE%"=="" set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%VSWHERE%" (
+  echo Not found VSWHERE.exe!
+  echo Cannot setup environment.
+  goto :EOF
+)
+
+for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set VSInstallDir=%%i
+)
+echo Visual studio installation folder is: %VSInstallDir%
+call "%VSInstallDir%\common7\tools\vsmsbuildcmd.bat"
+
 if "%~1"=="" (msbuild build.proj) else (msbuild -target:%1 build.proj)
